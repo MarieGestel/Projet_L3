e35031c6d7043d160cf4269998552166cb1fc2e7
Léa Béchet <lea.lelette@hotmail.fr>
2022-03-14 14:13:00 +0100

    fichier connecter.php pour lier connexion a la BD

---
+++ Code site/connecter.php
@@ -0,0 +1,34 @@
+<?php
+ require('bd.php');
+	$bdd = getBD() ; 
+
+	if($_POST['mail']=="" || $_POST['motdepasse']==""){
+		echo '<meta http-equiv="refresh" content="0; url=connexion.php?mail='.$_POST["mail"].'"/>';
+	}
+
+	else{
+	
+		$var=$bdd->query('select count(*) from clients where mail="'.$_POST['mail'].'" AND mdp="'.$_POST['motdepasse'].'"');
+		
+		$result=0;
+		while ($ligne = $var ->fetch()){
+		$result=$ligne[0];
+		}
+		$var->closeCursor();
+		
+		if($result==0){
+			echo '<meta http-equiv="refresh" content="0; url=connexion.php"/>';
+		}
+
+		else{
+				$rep = $bdd->query('select * from clients WHERE mail="'.$_POST['mail'].'" AND mdp="'.$_POST['motdepasse'].'"');
+				while ($ligne = $rep ->fetch()){
+					session_start();
+					$_SESSION['clients']=array($ligne['id_clients'], $ligne['nom'], $ligne['prenom'], $ligne['mail'], $ligne['sexe'],$ligne['adresse'], $ligne['numero'], $ligne['motdepasse']);
+				}
+				$rep->closeCursor();
+				echo '<meta http-equiv="refresh" content="0; url=index.php"/>';
+				
+			}
+		}
+?>
