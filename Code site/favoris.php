d0103757ba665a74dcf134ad3e44226bbf050a12
Marie Gestel <marie.egestel@gmail.com>
2022-03-14 14:04:10 +0100

    nouveau fichier favoris

---
+++ Code site/favoris.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include('bd.php');  
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type"
+		content="text/html; charset=UTF-8" />
+		
+		<link rel="stylesheet" href="styles.css"
+		type="text/css" media="screen" />
+		
+		<title>Medic'Info</title>
+	</head>
+	
+	<body id='medicament'>
+		
+	<p id="inscriptionConnexion">
+	<a 	href="deconnexion.html" > Se déconnecter </a>
+
+	
+	<h1> 	<a 	id="contact"href="index.html" > Medic'Info </a>  </h1>
+
+    </body>
+	
+	
+	
+	
+    </html>
