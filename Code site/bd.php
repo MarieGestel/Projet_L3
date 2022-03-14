392a66584a95e1e77a122af46aff2d7b2eceafd8
Marie Gestel <marie.egestel@gmail.com>
2022-03-14 15:11:23 +0100

    modification fichiers

--- Code site/bd.php
+++ Code site/bd.php
@@ -1,7 +1,7 @@
 
     <?php
             function getBDMarie(){
-                $bdd = new PDO('mysql:host=localhost:8889;dbname=BDD_medicament;charset=utf8', 'root', 'root');
+                $bdd = new PDO('mysql:host=localhost;dbname=BDD_medicament;charset=utf8', 'root', 'root');
                 return $bdd;
             }
 
