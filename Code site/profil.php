2c6f8a02cdef738b676dd2586991cdd90ccc42c3
Marie Gestel <marie.egestel@gmail.com>
2022-03-20 23:59:02 +0100

    creation profil

---
+++ Code site/profil.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+ 	require('bd.php');
+	$bdd = getBDMarie() ; 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type"
+		content="text/html; charset=UTF-8" />
+		
+		<link rel="stylesheet" href="styles.css"
+		type="text/css" media="screen" />
+		
+		
+		<title>Medic'Info</title>
+	</head>
+	
